6d07e9ff003c3affc38987bc45c9aaaf1bd47582
Jon Hill <jon.hill@york.ac.uk>
2026-02-01 08:58:36 +0000

    some minor linting fixes

--- test/test_tides.py
+++ test/test_tides.py
@@ -1,9 +1,8 @@
-import pytest
 import sys
 import os
 sys.path.insert(0,os.pardir)
 sys.path.insert(0,".")
-from tidal_analysis import *
+import pytest
 from pylint.lint import Run
 from pylint.reporters import CollectingReporter
 from dataclasses import asdict
@@ -11,6 +10,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import pytz
+from tidal_analysis import *
 
 @pytest.fixture
 def data_dir():
@@ -18,10 +18,10 @@ def data_dir():
     return Path(__file__).parent / "data"
 
 class TestTidalAnalysis():
-    
+
     def test_reading_data(self, data_dir):
         tidal_file = os.path.join(data_dir, "1947ABE.txt")
-        
+
         data = read_tidal_data(tidal_file)
         assert "Sea Level" in data.columns
         assert type(data.index) == pd.core.indexes.datetimes.DatetimeIndex
@@ -32,14 +32,12 @@ class TestTidalAnalysis():
         # check for M, N and T data; should be NaN
         assert data['Sea Level'].isnull().any()
         assert pd.api.types.is_float_dtype(data['Sea Level'])
-        
+
         # check for error on unknown file
         with pytest.raises(FileNotFoundError):
             read_tidal_data("missing_file.dat")
 
-    
     def test_join_data(self, data_dir):
-        
 
         gauge_files = [os.path.join(data_dir, '1946ABE.txt'),
                        os.path.join(data_dir, '1947ABE.txt')]
@@ -59,17 +57,16 @@ class TestTidalAnalysis():
         # check you get a fail if two incompatible dfs are given
         data2.drop(columns=["Sea Level","Time"], inplace=True)
         data = join_data(data1, data2)
-        
 
     def test_extract_year(self, data_dir):
-        
+
         gauge_files = [os.path.join(data_dir, '1946ABE.txt'),
                        os.path.join(data_dir, '1947ABE.txt')]
 
         data1 = read_tidal_data(gauge_files[1])
         data2 = read_tidal_data(gauge_files[0])
         data = join_data(data1, data2)
-        
+
         year1947 = extract_single_year_remove_mean("1947",data)
         assert "Sea Level" in year1947.columns
         assert type(year1947.index) == pd.core.indexes.datetimes.DatetimeIndex
@@ -78,7 +75,6 @@ class TestTidalAnalysis():
         mean = np.mean(year1947['Sea Level'])
         # check mean is near zero
         assert mean == pytest.approx(0)
-
         # check something sensible when a year is given that doesn't exist
 
     def test_extract_section(self, data_dir):
@@ -89,7 +85,7 @@ class TestTidalAnalysis():
         data1 = read_tidal_data(gauge_files[1])
         data2 = read_tidal_data(gauge_files[0])
         data = join_data(data1, data2)
-        
+
         year1946_47 = extract_section_remove_mean("19461215", "19470310", data)
         assert "Sea Level" in year1946_47.columns
         assert type(year1946_47.index) == pd.core.indexes.datetimes.DatetimeIndex
@@ -107,7 +103,6 @@ class TestTidalAnalysis():
         mean = np.mean(data_segment['Sea Level'])
         # check mean is near zero
         assert mean == pytest.approx(0)
-
         # check something sensible is done when dates are formatted correctly.
 
     def test_correct_tides(self, data_dir):
@@ -130,7 +125,6 @@ class TestTidalAnalysis():
         assert amp[0] == pytest.approx(1.307,abs=0.1)
         assert amp[1] == pytest.approx(0.441,abs=0.1)
 
-
     def test_linear_regression(self, data_dir):
 
         gauge_files = [os.path.join(data_dir, '1946ABE.txt'),
@@ -141,10 +135,8 @@ class TestTidalAnalysis():
         data = join_data(data1, data2)
 
         slope, p_value = sea_level_rise(data)
-        
         assert slope == pytest.approx(2.94e-05,abs=1e-7)
         assert p_value == pytest.approx(0.427,abs=0.1)
-        
 
     def test_lint(self):
         files =  ["tidal_analysis.py"]
@@ -163,11 +155,11 @@ class TestTidalAnalysis():
         print("Score: " + str(score))
         line_format = "{path}:{line}:{column}: {msg_id}: {msg} ({symbol})"
         for error in report.messages:
-            print(line_format.format(**asdict(error)))   
+            print(line_format.format(**asdict(error)))
 
         score_thresholds = [3, 5, 7, 9]
         error_thresholds = [500, 250, 100, 50, 10, 0]
-        
+
         results = {"pass": 0, "fail": 0}
 
         for t in score_thresholds:
@@ -177,14 +169,14 @@ class TestTidalAnalysis():
                 results["fail"] += 1
 
         for t in error_thresholds:
-            if nErrors <= t: 
+            if nErrors <= t:
                 results["pass"] += 1
             else:
                 results["fail"] += 1
 
         print(f"You passed {results['pass']} out of {len(score_thresholds) + \
                 len(error_thresholds)} lint checks.")
-        
+
         # Finally, trigger a failure if they didn't get a perfect score
         # or just assert results["fail"] == 0
         assert results["fail"] == 0, f"You failed {results['fail']} lint tests."
@@ -194,18 +186,20 @@ class TestRegression():
     def test_whitby_regression(self):
 
         from subprocess import run
-        result = run(["python3","tidal_analysis.py","-v","data/whitby"], capture_output=True, check=True)
+        result = run(["python3","tidal_analysis.py","-v","data/whitby"],
+                     capture_output=True, check=True)
         assert len(result.stdout) > 25
 
     def test_aberdeen_regression(self):
 
         from subprocess import run
-        result = run(["python3","tidal_analysis.py","--verbose","data/aberdeen"], capture_output=True, check=True)
+        result = run(["python3","tidal_analysis.py","--verbose","data/aberdeen"],
+                     capture_output=True, check=True)
         assert len(result.stdout) > 25
 
     def test_dover_regression(self):
 
         from subprocess import run
-        result = run(["python3","tidal_analysis.py","data/dover"], capture_output=True, check=True)
+        result = run(["python3","tidal_analysis.py","data/dover"],
+                     capture_output=True, check=True)
         assert len(result.stdout) > 25
-
